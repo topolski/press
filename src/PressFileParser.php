96fb442c325576dd693399fbdc27c1db50da061f
Milanko Topolski <tope1991@gmail.com>
2019-10-12 14:58:53 +0200

    added PressFileParser class with tests

---
+++ src/PressFileParser.php
@@ -0,0 +1,33 @@
+<?php
+
+
+namespace topolski\Press;
+
+
+use Illuminate\Support\Facades\File;
+
+class PressFileParser
+{
+    protected $filename;
+    protected $data;
+
+    public function __construct($filename)
+    {
+        $this->filename = $filename;
+
+        $this->splitFile();
+    }
+
+    public function getData()
+    {
+        return $this->data;
+    }
+
+    protected function splitFile()
+    {
+        preg_match('/^\-{3}(.*?)\-{3}(.*)/s',
+            File::get($this->filename),
+            $this->data
+        );
+    }
+}
---
+++ tests/Feature/PressFileParserTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace topolski\Press\Tests;
+
+use Orchestra\Testbench\TestCase;
+use topolski\Press\PressFileParser;
+
+class PressFileParserTest extends TestCase
+{
+    /** @test */
+    public function the_head_and_body_gets_split()
+    {
+        $pressFileParser = (new PressFileParser(__DIR__.'/../blogs/MarkFile1.md'));
+
+        $data = $pressFileParser->getData();
+
+        $this->assertStringContainsString('title: My Title', $data[1]);
+        $this->assertStringContainsString('description: Description here', $data[1]);
+        $this->assertStringContainsString('Blog post body here', $data[2]);
+    }
+}

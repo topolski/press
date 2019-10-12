6ce7166329677eb0d2aa7d2a23f96e39cfbe39c8
Milanko Topolski <tope1991@gmail.com>
2019-10-12 14:26:10 +0200

    added autoload and test config file

---
+++ tests/Feature/InitialTest.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace topolski\Press\Tests;
+
+
+use Orchestra\Testbench\TestCase;
+
+class InitialTest extends TestCase
+{
+    /** @test */
+    public function my_first_test()
+    {
+        $this->assertTrue(true);
+    }
+}
